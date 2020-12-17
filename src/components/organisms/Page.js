209c5a9b1ba3a6347697d20feda8fff4007b2feb
Kevin <nghe0001@algonquinlive.com>
2020-12-17 10:15:03 -0800

    btn added

--- src/components/organisms/Page.js
+++ src/components/organisms/Page.js
@@ -5,6 +5,9 @@ import { Footer } from "./Footer";
 import { useTranslation } from "react-i18next";
 import { useSelector, useDispatch } from "react-redux";
 import { changeLanguageCreator, LANGUAGES } from "../../redux/actions";
+import { ActionButton } from "../atoms/ActionButton";
+import GitInfo from "react-git-info/macro";
+import moment from "moment";
 
 /**
  * page component complete with canada.ca header and footer
@@ -14,6 +17,7 @@ export function Page(props) {
   const language = useSelector((state) => state.language);
   const dispatch = useDispatch();
   const { t } = useTranslation();
+  const gitInfo = GitInfo();
 
   let languageButtonHandler = () => {
     if (language === "en") {
@@ -34,8 +38,28 @@ export function Page(props) {
       <div className="w-full md:w-2/3 m-0 md:mr-auto md:ml-auto p-4 md:p-0">
         {props.children}
       </div>
-
       <div className="w-full flex flex-col flex-grow justify-end">
+        <div className="w-full md:w-2/3 m-0 md:mr-auto md:ml-auto p-4 md:p-0 mmflex sm:flex-col justify-between  ">
+          <ActionButton
+            className={"shadow-none hover:text-black"}
+            id="report"
+            icon="mt-1 mr-1 icon-play_arrow"
+            text="Report a problem or mistake on this page"
+            invert="true"
+            // dataTestId="envelopIcon"
+          />
+          <ActionButton
+            className={"shadow-none hover:text-black"}
+            id="report"
+            icon="mt-1 mr-1 icon-share"
+            text="Share this page"
+            invert="true"
+            // dataTestId="envelopIcon"
+          />
+        </div>
+        <div className="w-full md:w-2/3 m-0 md:mr-auto md:ml-auto p-4 md:p-0 md:mt-6  md:mb-2">
+          <p> Date Modified: {moment(gitInfo.commit.date).format("L")}</p>
+        </div>
         <Footer
           contactLink={t("contactLink")}
           contactText={t("contactText")}
